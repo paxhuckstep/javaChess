44730ac02d06c9bb42cef292057d08ac2b76e842
paxhuckstep <paxhuckstep@gmail.com>
2025-09-30 19:18:23 -0600

    queen candidate moves r done

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -1,7 +1,30 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queen extends Piece {
+
     public Queen(boolean isWhite) {
-        super(isWhite);  // call Piece constructor
+        super(isWhite);
     }
+
+    @Override
+    public String getSay() {
+        return " I am the Queen, your slutty queen";
+    }
+
+    @Override
+    public List<int[]> getCandidateMoves(int row, int column) {
+        List<int[]> candidateMoves = new ArrayList<>();
+
+        Rook queenStraights = new Rook(this.isWhite);
+        candidateMoves.addAll(queenStraights.getCandidateMoves(row, column));
+
+        Bishop queenDiagonals = new Bishop(this.isWhite);
+        candidateMoves.addAll(queenDiagonals.getCandidateMoves(row, column));
+
+        return candidateMoves;
+    }
+
 }
